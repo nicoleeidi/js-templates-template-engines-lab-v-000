f1084718eb003c578b1d8a6300ede3be1ad16dde
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 01:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ function createPost(){
 function postComment() {
   var commenter = document.getElementById("comment").value;
   var comment = document.getElementById("commentText").value;
-
-  //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
-
-  //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
-  //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
   var commentTemplate = document.getElementById("comment-template").innerHTML;
-
-  //create template function
   var templateFn = _.template(commentTemplate);
-
   var commentsDiv = document.getElementById("comments");
-
-  //execute template function with JSON object for the interpolated values
   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
-
-  //append rather than replace!
   commentsDiv.innerHTML += templateHTML;
+}
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
