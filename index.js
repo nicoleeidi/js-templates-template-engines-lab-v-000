d88be34567ff160ace6ba3ac5c4be071e75d8b6c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-01 01:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function createPost(){
 function postComment(){
   var commenter = document.getElementById("commenterName").value;
    var comment = document.getElementById("commentText").value;
-   
+ var commentTemplate = document.getElementById("comment-template").innerHTML;
 }
